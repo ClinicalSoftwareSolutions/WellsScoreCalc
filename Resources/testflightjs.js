e6a16d3a3d6f43938c8a3a9f4f9ad63d0771b864
Neville Dastur <pcsdev@medicsnet.co.uk>
2012-12-01 12:07:59 +0000

    Update testflight js module

--- Resources/testflightjs.js
+++ Resources/testflightjs.js
@@ -2,12 +2,31 @@
  * @author Neville Dastur
  * 
  * This Javascript module is for use with the Titanium Appcelerator
- * TestFlightTi module
+ * TestFlightTi module found in the MarketPlace
+ * https://marketplace.appcelerator.com/apps/4114
+ * 
+ * Copyright 2012 Neville Dastur
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ * 
  */
 
+var _takeOffDone = false;
 var _osname = Ti.Platform.osname;
 var is_iOS = _osname === 'ipad' || _osname === 'iphone';
-var TF = require('com.clinsoftsolutions.testflight');
+if(is_iOS) {
+	var TF = require('com.clinsoftsolutions.testflight');
+}
 
 exports.takeOff = function(_teamToken, _testingFlag) {
 	if(is_iOS) {
@@ -42,6 +61,7 @@ exports.testException = function() {
 };
 
 exports.passCheckpoint = function(_msg) {
+	if(!_takeOffDone) return;
 	if(is_iOS) {
 		TF.passCheckpoint(_msg);
 	}
@@ -51,6 +71,7 @@ exports.passCheckpoint = function(_msg) {
 };
 
 exports.launchFeedbackView = function() {
+	if(!_takeOffDone) return;
 	if(is_iOS) {
 		TF.launchFeedbackView();
 	}
@@ -60,6 +81,7 @@ exports.launchFeedbackView = function() {
 };
 
 exports.submitFeedback = function(_text) {
+	if(!_takeOffDone) return;
 	if(is_iOS) {
 		TF.submitFeedback(_text);
 	}
@@ -69,6 +91,7 @@ exports.submitFeedback = function(_text) {
 };
 
 exports.remoteLog = function(_text) {
+	if(!_takeOffDone) return;
 	if(is_iOS) {
 		TF.remoteLog(_text);
 	}
@@ -78,6 +101,7 @@ exports.remoteLog = function(_text) {
 };
 
 exports.addCustomEnvironmentInformation = function(_key, _value) {
+	if(!_takeOffDone) return;
 	if(is_iOS) {
 		TF.addCustomEnvironmentInformation(_key, _value);
 	}
